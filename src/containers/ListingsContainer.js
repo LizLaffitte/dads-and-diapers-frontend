148652a93b5d3a360c8caedf7bd0fbe02249a5df
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-07 14:09:31 -0400

    Listing imported into ListingsContainer.

--- src/containers/ListingsContainer.js
+++ src/containers/ListingsContainer.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import Listings from '../components/Listings'
+import { connect } from 'react-redux'
 
 class ListingsContainer extends Component {
     render(){
-        return <h1>Listings Container</h1>
+        return (
+            <div>
+                <h1>Listings Container</h1>
+                <Listings />
+            </div>
+        )
     }
 }
 
-export default ListingsContainer
+export default connect()(ListingsContainer)
