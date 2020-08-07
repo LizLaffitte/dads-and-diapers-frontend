99af3514c54d92a87f943ad3efa3d5d06abfae9c
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-07 16:12:50 -0400

    Added bare Listing component.

---
+++ src/components/Listing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Listing extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Listing
