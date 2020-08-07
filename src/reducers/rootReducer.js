5af76f5b80862000e21015403ff9495bfd763110
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-07 15:30:04 -0400

    Swtich case and default case for two reducers.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,23 @@
-function rootReducer(state=[], action){
-    return state
+import { combineReduers } from 'redux'
+
+function manageListings(state=[], action){
+    switch(action.type){
+        default:
+            return state
+    }
+    
 }
+
+function manageReviews(state=[], action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReduers({
+    listings: manageListings,
+    reivews: manageReviews
+})
 
 export default rootReducer
