4f8cdd962e9d0aba47a11bd064696db714269e39
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-06 20:50:19 -0400

    Basic class components for Listings and ListingsContainer. Added in app.js

---
+++ src/components/Listings.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Listings extends Component {
+    render(){
+        return <div></div>
+    }
+}
+
+export default Listings
--- src/containers/ListingsContainer.js
+++ src/containers/ListingsContainer.js
@@ -1,2 +1,9 @@
+import React, { Component } from 'react'
 
+class ListingsContainer extends Component {
+    render(){
+        return <h1>Listings Container</h1>
+    }
+}
 
+export default ListingsContainer
