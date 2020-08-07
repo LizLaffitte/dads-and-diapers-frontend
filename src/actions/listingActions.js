666e60c991eb79769f4e7f98fc19258149ed2d39
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-07 15:31:16 -0400

    removed dispatch line in fetchListings.

--- src/actions/listingActions.js
+++ src/actions/listingActions.js
@@ -1,6 +1,5 @@
 export const fetchListings = () => {
-    return (dispatch) => {
-        dispatch({type: 'LOADING_LISTINGS'})
+    return dispatch => {
         fetch('http://localhost:3000/api/v1/listings')
         .then(response => response.json())
         .then(listingData => console.log(listingData))
