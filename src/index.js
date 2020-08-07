cf559b605182f8c791c036ae3bd3b8ced5311e96
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-07 14:31:52 -0400

    Removed serviceWorkers code from index.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-// import * as serviceWorker from 'redux'
 import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
@@ -17,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+
