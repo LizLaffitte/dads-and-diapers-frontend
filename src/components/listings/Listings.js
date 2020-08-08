8e0e2fff9c8c51bac95cf4ec11142a99ffa53b9f
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 13:49:38 -0400

    Implemented render in Route for ListingInput

--- src/components/listings/Listings.js
+++ src/components/listings/Listings.js
@@ -4,6 +4,7 @@ import Listing from './Listing'
 class Listings extends Component {
    renderListings = () => {
        return this.props.listings.map(listing => {
+           console.log(listing)
            return (<Listing key={listing.id} listing={listing} />)
        })
    }
