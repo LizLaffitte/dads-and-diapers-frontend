87af890670a0caa2c609104069fbdd27d514f048
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 10:52:46 -0400

    createListing action.

--- src/actions/listingActions.js
+++ src/actions/listingActions.js
@@ -18,10 +18,27 @@ export const fetchListings = () => {
             method: "GET",
             headers: {
               "Content-Type": "application/json"
-            },
+            }
           })
         .then(response => response.json())
         .then(listingData => dispatch({type: 'ADD_LISTINGS', listings: listingData}))
+    }
+}
+
+export const createListing = (listingFormData) => {
+    return dispatch => {
+        const listing = {
+            name: listingFormData.name,
+            address: listingFormData.address
+        }
+        return fetch('http://localhost:3000/api/v1/listings', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            }, body: JSON.stringify(listing)
+          })
+        .then(response => response.json())
+        .then(listingData => dispatch(addListing(listingData)))
     }
 }
 
