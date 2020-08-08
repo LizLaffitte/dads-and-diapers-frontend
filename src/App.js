7a0ce2e3787e28c23b92ffc6055ea8eabb103758
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 13:20:51 -0400

    Added react-router-dom, first route."

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React , { Component } from 'react';
 import './App.css';
 import ListingsContainer from './containers/ListingsContainer'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 class App extends Component {
 
   render (){
     return (
       <div className="App">
         <header className="App-header">
-          <ListingsContainer />
+          <Router>
+            <div>
+              <Route path='/' component={ListingsContainer}  />
+            </div>
+            </Router>
         </header>
       </div>
     )
