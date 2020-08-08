1758b64075cfd7eb9f833561940445c9d4561e3b
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 10:11:55 -0400

    Added address input and label, submit button to ListingInput form.

--- src/components/listings/ListingInput.js
+++ src/components/listings/ListingInput.js
@@ -10,14 +10,14 @@ class ListingInput extends Component {
         const {name, address} = this.state
         return (
             <form>
-                <label>Listing Name:</label><br />
+                <h2>Add a New Listing</h2>
+                <label>Name:</label><br />
                 <input type="text" value={name} /><br /><br />
-
+                <label>Address:</label>
                 <br />
-
+                <input type="text" valut={address} />
                 <br /><br />
-                
-
+                <input type="submit" />
             </form>
         )
     }
