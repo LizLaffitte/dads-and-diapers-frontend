d023f5ad5c0ffe57a94e5840fee67dcb1e1c148e
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 11:43:43 -0400

    Passing listing to createListing.

--- src/components/listings/ListingInput.js
+++ src/components/listings/ListingInput.js
@@ -14,6 +14,7 @@ class ListingInput extends Component {
         e.preventDefault()
         const listing = {name: this.state.name, address: this.state.address}
         this.props.createListing(listing)
+        this.setState({name: '', address: ''})
     }
 
     render(){
