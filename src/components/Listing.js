b7a3ee5fc62d7adeeea58c10044d85ee2e48e43d
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-08 09:25:18 -0400

    Render Listing info including address, chance of changing table.

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react'
 class Listing extends Component {
     
     render(){
-        const {name} = this.props.listing
+        const {name, address, mens_chance, womens_chance} = this.props.listing
         return(
             <div>
                 <h3>{name}</h3>
+                <p>{address}</p>
+                <div className="rating-results">
+                <div>{mens_chance}% Men's</div>
+                <div>{womens_chance}% Women's</div>
+                </div>
             </div>
         )
     }
