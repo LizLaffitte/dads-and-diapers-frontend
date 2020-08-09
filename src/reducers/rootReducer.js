b08e10eaeefb0032b08dc63303f20f9929b79ad9
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 18:10:04 -0400

    Added key for loginForm in store.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import manageListings from './manageListings'
 import manageReviews from './manageReviews'
 import currentUser from './currentUser'
+import loginForm from './loginForm'
 
 const rootReducer = combineReducers({
     listings: manageListings,
     reviews: manageReviews,
-    currentUser: currentUser
+    currentUser: currentUser,
+    loginForm: loginForm
 })
 
 export default rootReducer
