33fbba50834d977f909bfe3533f0f91459569b23
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 16:09:44 -0400

    Updated onSubmit function.

--- src/containers/EditListingFormWrapper.js
+++ src/containers/EditListingFormWrapper.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import ListingForm from '../components/listings/ListingForm'
 import { connect } from 'react-redux'
 import {updateListing} from '../actions/listingActions'
+import { testListing } from '../actions/listingActions'
 
 class EditListingFormWrapper extends Component {
 
     handleOnSubmit = (state) => {
-        this.props.updateListing({name: state.name, address: state.address})
+        this.props.updateListing({name: state.name, address: state.address}, this.props.listing.id)
     }
 
     render(){
