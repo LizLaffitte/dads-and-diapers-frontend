917cb58e95f0aba4bc0dd336c0a2e6a32f20c516
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 18:27:59 -0400

    Changed LoginForm to class component, gave it internal state.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import { connect } from 'react-redux'
-const LoginForm = ({username, password}) => {
-    return(
-        <form onSubmit={undefined}>
-            <label>Username:</label><br />
-            <input type="text" value={username} onChange={undefined} name="username" />
-            <br /><br />
-            <label>Password:</label><br />
-            <input type="password" value={password} name="password" onChange={undefined} />
-            <br /><br />
-            <input type="submit" value="Log In"  />
-        </form>
-    )
-}
-const mapStateToProps = state => {
-    return {
-        username: state.loginForm.user,
-        password: state.loginForm.password
+import React, { Component } from 'react'
+
+class LoginForm extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+        this.props.login(this.state)
+    }
+    render() {
+        const {username, password} = this.state
+        return(
+            <form onSubmit={this.handleOnSubmit}>
+                <label>Username:</label><br />
+                <input type="text" value={username} onChange={this.handleOnChange} name="username" />
+                <br /><br />
+                <label>Password:</label><br />
+                <input type="password" value={password} name="password" onChange={this.handleOnChange} />
+                <br /><br />
+                <input type="submit" value="Log In"  />
+            </form>
+        )
     }
 }
-export default connect(mapStateToProps, )(LoginForm)
+export default LoginForm
