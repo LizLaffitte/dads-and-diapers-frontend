275561e08c9a3aae52ce134a83d0a09fce38730f
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 16:04:42 -0400

    Added testListing to reducer for debugging.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,10 +3,15 @@ import { combineReducers } from 'redux'
 function manageListings(state=[], action){
     switch(action.type){
         case 'ADD_LISTINGS':
+            console.log(action.listings)
             return action.listings
         case 'ADD_LISTING':
             return [...state, action.listing]
-
+        case 'EDIT_LISTING':
+            return state.map(listing => listing.id == action.listing.id ? action.listing : listing)
+        case 'TEST_LISTING':
+            console.log("This is a test")
+            return state
         default:
             return state
     }
