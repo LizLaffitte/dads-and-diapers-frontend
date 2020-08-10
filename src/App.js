4e3d86feb0f9d700818837db1f604b56952fe3c9
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 18:20:19 -0400

    Remove unneccesary ListingsContainer import.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React , { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import HeaderContainer from './containers/HeaderContainer'
-import ListingsContainer from './containers/ListingsContainer'
 import MainContainer from './containers/MainContainer'
 
 class App extends Component {
