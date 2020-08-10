d9c0902d0f9ae364f93f4b5aa494ad1cb366a190
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 21:17:15 -0400

    Get current user when App mounts.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import ListingsContainer from './containers/ListingsContainer'
 import LoginForm from './components/LoginForm'
-import {login} from './actions/userActions'
+import {login, getCurrentUser} from './actions/userActions'
 class App extends Component {
-
+  componentDidMount() {
+    this.props.agetCurrentUser()
+  }
   render (){
     return (
       <div className="App">
@@ -25,4 +27,4 @@ class App extends Component {
 
 
 
-export default connect(null, {login})(App)
+export default connect(null, {login, getCurrentUser})(App)
