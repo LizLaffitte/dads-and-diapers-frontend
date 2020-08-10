3ece0bb9c5c7e1d3f701d014cd1a25f84925389e
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-09 21:19:00 -0400

    Async action creators for login and getcurrentUser.

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -9,14 +9,48 @@ export const setCurrentUser = user => {
 //async
 export const login = credentials => {
     return dispatch => {
-        return fetch('http://localhost:3000/login', {
+        return fetch('http://localhost:3001/login', {
+            credentials: 'include',
             method: "POST",
             headers: {
               "Content-Type": "application/json"
             },
-            body: JSON.stringify({username: "Liz", password:"dinos"})
+            body: JSON.stringify(credentials)
           })
         .then(response => response.json())
-        .then(userData => dispatch(setCurrentUser(userData)))
+        .then(userData => {
+            if(userData.errors){
+                console.log(userData.errors)
+            } else {
+                console.log(userData)
+                dispatch(setCurrentUser(userData.data))
+            }
+            
+        })
+        .catch(console.log())
+    }
+}
+
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch('http://localhost:3001/get_current_user', { 
+            credentials: "include",
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+        .then(response => response.json())
+        .then(userData => {
+            if(userData.errors){
+                console.log(userData.errors)
+            } else {
+                console.log(userData)
+                dispatch(setCurrentUser(userData.data))
+            }
+            
+        })
+        .catch(console.log("test"))
     }
 }
