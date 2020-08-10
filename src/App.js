eeafe8cadf1fd5f68d8500b7af1e6cf42027b449
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 13:09:37 -0400

    Disconnect App from the store. Move store connection to each container component, starting with HeaderContainer. Allow us to have a clean App component that is not concerned with state.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import React , { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import { connect } from 'react-redux'
+import HeaderContainer from './containers/HeaderContainer'
 import ListingsContainer from './containers/ListingsContainer'
-import LoginForm from './components/LoginForm'
-import LogoutForm from './components/LogoutForm'
-import {login, getCurrentUser, logout} from './actions/userActions'
 
 
 class App extends Component {
-  componentDidMount() {
-    this.props.getCurrentUser()
-  }
 
-  logButtonRender = () => {
-    return (
-      this.props.currentUser ? <LogoutForm logout={this.props.logout} /> : <LoginForm login={this.props.login} />
-    )
-  }
   render (){
     return (
       <div className="App">
-        <header className="App-header">
           <Router>
             <div>
               <Route path='/' component={ListingsContainer}  />
             </div>
             </Router>
-            {this.logButtonRender()}
-        </header>
+            <HeaderContainer />
       </div>
     )
   }
 }
 
-const mapStateToProps = ({currentUser}) => ({currentUser})
 
-export default connect(mapStateToProps, {login, getCurrentUser, logout})(App)
+
+export default App
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import LoginForm from '../components/LoginForm'
+import LogoutForm from '../components/LogoutForm'
+import {login, getCurrentUser, logout} from '../actions/userActions'
+
+class HeaderContainer extends Component {
+    componentDidMount() {
+        this.props.getCurrentUser()
+      }
+
+    buttonRender = () => {
+        return (
+          this.props.currentUser ? <LogoutForm logout={this.props.logout} /> : <LoginForm login={this.props.login} />
+        )
+      }
+
+    render(){
+        return(
+            <header>
+                <div>Logo Here</div>
+                <div>{this.buttonRender()}</div>
+            </header>
+        )
+    }
+}
+
+const mapStateToProps = ({currentUser}) => ({currentUser})
+
+export default connect(mapStateToProps, {login, getCurrentUser, logout})(HeaderContainer)
