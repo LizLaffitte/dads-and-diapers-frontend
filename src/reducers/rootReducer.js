9da639f88342ce96818157af80fef2a17ebf4d5e
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-11 15:38:56 -0400

    Make currentOptions available in the store.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux'
 import manageListings from './manageListings'
 import manageReviews from './manageReviews'
 import currentUser from './currentUser'
+import currentOptions from './currentOptions'
+import manageForms from './manageForms'
 
 const rootReducer = combineReducers({
     listings: manageListings,
     reviews: manageReviews,
-    currentUser: currentUser
+    currentUser,
+    currentOptions,
+    forms: manageForms
 })
 
 export default rootReducer
