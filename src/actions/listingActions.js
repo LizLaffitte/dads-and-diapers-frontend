085e20f1f57a6a148b33c37c004a3d163732c9a6
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-11 15:57:57 -0400

    Send fetch to correct URL.

--- src/actions/listingActions.js
+++ src/actions/listingActions.js
@@ -79,7 +79,7 @@ export const createListing = (listingFormData) => {
 export const updateListing = (listingUpdateData, listingId) => {
     return dispatch => {
         const listing = {listing: listingUpdateData}
-        return fetch(`http://localhost:3000/api/v1/listings/${listingId}`, {
+        return fetch(`http://localhost:3001/api/v1/listings/${listingId}`, {
             method: "PATCH",
             headers: {
               "Content-Type": "application/json"
