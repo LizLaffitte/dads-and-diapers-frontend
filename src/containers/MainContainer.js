b6e8a21e1a2cde6c01bce5f0d474ea6989cabf27
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-10 20:38:29 -0400

    Enable MainContainer to redirect logged in users attempting to access to login form.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect, useHistory} from 'react-router-dom'
 import LoginForm from '../components/userForms/LoginForm'
 import SignupForm from '../components/userForms/SignupForm'
-import {login, signup} from '../actions/userActions'
+import {login, signup, getCurrentUser} from '../actions/userActions'
 import ListingsContainer from '../containers/ListingsContainer'
 
 class MainContainer extends Component {
+    componentDidMount() {
+        this.props.getCurrentUser()
+    }
+
+    loggedIn = () =>{
+        return this.props.currentUser
+    }
     render(){
         return(
             <main>
                     <Switch>
-                        <Route exact path='/login' render={props => <LoginForm {...props} login={this.props.login} />} />
+                        <Route exact path='/login' >
+                            {this.loggedIn() ? <Redirect to="/listings" /> : <LoginForm login={this.props.login}  />}
+                        </Route>
                         <Route exact path='/signup' render={props => <SignupForm {...props} signup={this.props.signup} />} />
                         <Route exact path='/listings' component={ListingsContainer} />
                     </Switch>
@@ -20,5 +29,5 @@ class MainContainer extends Component {
         )
     }
 }
-
-export default connect(null, {login, signup})(MainContainer)
+const mapStateToProps = ({currentUser}) => ({currentUser})
+export default connect(mapStateToProps, {login, signup, getCurrentUser})(MainContainer)
