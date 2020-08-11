fbb7ce6439b93314bd0992f3d4527fb69c37c55f
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-11 17:35:38 -0400

    Add links to individual listings, remove problematic edit link.

--- src/components/listings/Listing.js
+++ src/components/listings/Listing.js
@@ -5,14 +5,15 @@ class Listing extends Component {
     
     render(){
         const {name, address, mens_chance, womens_chance} = this.props.listing.attributes
+        const {id} = this.props.listing
         return(
             <div>
-                <h3>{name}</h3>
+                <Link to={`/listings/${id}`}><h3>{name}</h3></Link>
                 <p>{address}</p>
                 <div className="rating-results">
                 <div>{mens_chance}% Men's</div>
                 <div>{womens_chance}% Women's</div>
-                <Link to={`/listings/${this.props.listing.id}/edit`}>Edit</Link>
+                {/* <Link to={`/listings/${this.props.listing.id}/edit`}>Edit</Link> */}
                 </div>
             </div>
         )
