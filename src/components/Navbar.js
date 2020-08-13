5a31f3525922d4577d418b31a3693a14910c3852
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-12 20:46:17 -0400

    Allow using to click link to add new listing instead of visiting URL directly.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
     render() {
         return(
            <nav>
-               <NavLink to="/listings">Listings</NavLink>
+               <NavLink to="/listings">Listings</NavLink> | <NavLink to="/listings/new">Add Listing</NavLink> 
            </nav>
         )
     }
