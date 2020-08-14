d55a8ab7075f87bb45870920b1b4cb6f19228197
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-13 21:02:52 -0400

    Create button component.

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Button(props){
+    return(
+        <div>
+        <Link to={props.link}><button>{props.text}</button></Link> 
+        
+    </div>
+    )
+}
+
+export default Button
