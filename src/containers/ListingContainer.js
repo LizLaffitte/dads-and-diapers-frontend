0e718f352042e6eb04a95d395fdf50058a2e9c2b
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-13 21:06:23 -0400

    Add className for styling purposed. Separate buttons from one component holding two buttons to a component that renders one button for more use cases.

--- src/containers/ListingContainer.js
+++ src/containers/ListingContainer.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-
 import Listing from '../components/listings/Listing'
 import Reviews from '../components/reviews/Reviews'
+import Button from '../components/Button'
 
 class ListingContainer extends Component {
     render() {
@@ -9,8 +9,10 @@ class ListingContainer extends Component {
         
         return(
         
-            <div class="listingcard">
+            <div className="listingcard">
                 <Listing listing={listing} key={listing.id} />
+                <Button link={`/listings/${listing.id}/edit`} text="Edit"/>
+                <Button link={`/listings/${listing.id}/reviews/new`} text="Add a Review"/>
                 <Reviews reviews={listing.attributes.reviews} />
             </div>
         )
