836657cfb9d9a575218b1ef86a1bfe9ca8076f98
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-14 21:28:01 -0400

    Change how reviews are passed to Reviews component, as the way the data is saved in the store has changed. Accessing associatedReviews by matching ids.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import Review from './Review'
 
+
 class Reviews extends Component {
-    
-    renderReviews = () => {
-        return this.props.reviews.map(review => {
-            return (<Review key={review.id} review={review} />)
-        })
-        
+
+    renderReviews = (reviews) => {
+            return reviews.map(review => {
+                    return (<Review key={review.id} review={review} user={this.associatedUser(review)} currentUser={this.props.currentUser} listing={this.props.listing} />)
+            })
     }
+
+    associatedUser = (review) => {
+        return this.props.users.find(user => user.id == review.attributes.user_id)
+    }
+        
     render()  {
+        const associatedReviews = this.props.reviews.filter(review => review.attributes.listing_id == this.props.listing.id)
         return (
             <div className="reviews"> 
              <h3>Reviews:</h3>          
-            {this.renderReviews()}
+            {this.renderReviews(associatedReviews)}
             
         </div>
         )
         
     }
 }
-
-export default Reviews
+const mapStateToProps = ({currentUser}) => ({currentUser})
+export default connect(mapStateToProps)(Reviews)
