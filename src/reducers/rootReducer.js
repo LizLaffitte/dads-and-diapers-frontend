a81e3afb0d7200d89ea8c126ecee8b07d9fa5981
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-14 21:33:43 -0400

    Add users key to store.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,10 +4,12 @@ import manageReviews from './manageReviews'
 import currentUser from './currentUser'
 import currentOptions from './currentOptions'
 import manageForms from './manageForms'
+import manageUsers from './manageUsers'
 
 const rootReducer = combineReducers({
     listings: manageListings,
     reviews: manageReviews,
+    users: manageUsers,
     currentUser,
     currentOptions,
     forms: manageForms
