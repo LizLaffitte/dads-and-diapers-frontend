1a2cd5a231710e3b10750d4d80eda4cad0955acc
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-14 21:32:38 -0400

    Allow Redux DevTools to trace actions.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
-const store = createStore(rootReducer,compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer,compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })))
 ReactDOM.render(
   <Provider store={store}>
     <App />
