b656a9d2adef0680199ecdf6f083a5852b4287ed
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-14 21:29:56 -0400

    Group Listing and Button components in one div, so they can be visually separated in the UI.

--- src/containers/ListingContainer.js
+++ src/containers/ListingContainer.js
@@ -2,22 +2,26 @@ import React, {Component} from 'react'
 import Listing from '../components/listings/Listing'
 import Reviews from '../components/reviews/Reviews'
 import Button from '../components/Button'
+import {connect} from 'react-redux'
 
 class ListingContainer extends Component {
+
+
     render() {
-        const {listing} = this.props
-        
+        const {listing, reviews, users} = this.props
         return(
         
-            <div className="listingcard">
-                <Listing listing={listing} key={listing.id} />
-                <Button link={`/listings/${listing.id}/edit`} text="Edit"/>
-                <Button link={`/listings/${listing.id}/reviews/new`} text="Add a Review"/>
-                <Reviews reviews={listing.attributes.reviews} />
+            <div className="listingcard flex space-btwn">
+                <div>
+                    <Listing listing={listing} key={listing.id} />
+                    <Button link={`/listings/${listing.id}/edit`} text="Edit"/>
+                    <Button link={`/listings/${listing.id}/reviews/new`} text="Add a Review"/>
+                </div>
+                <Reviews listing={listing} reviews={reviews} users={users}  />
             </div>
         )
     }
     
 }
-
-export default ListingContainer
+const mapStateToProps = ({reviews, users}) => ({reviews, users})
+export default connect(mapStateToProps)(ListingContainer)
