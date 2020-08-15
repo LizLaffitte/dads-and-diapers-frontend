e3a9d0993948d4e0aaf5507eb91ac35ad8b7c067
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-14 21:25:27 -0400

    Display review attributes when the Review component is rendered.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Button from '../Button'
 
+class Review extends Component  {
+    classDecision = (prop) => {
+        if(prop) {
+            return "green"
+        } else {
+            return "red"
+        }
+    }
 
-const Review = props =>  {
+    isOwner = (user) => {
+        if(this.props.currentUser && user.id.toString() === this.props.currentUser.id){
+            return <Button link={`/listings/${this.props.listing.id}/reviews/${this.props.review.id}/edit`} text="Edit Review" />
+        }
+    }
+        
+    render() {
+        const {womens, mens, working, user, clean, isolated} = this.props.review.attributes
         return(
             <div className="review">
-                Review
-                {console.log(props.review)}
+                {user.username}
+                <div>
+                <span className={this.classDecision(mens)}>Men's</span> | <span className={this.classDecision(womens)}>Women's</span> <br />
+                <span className={this.classDecision(isolated)}>Private</span> | <span className={this.classDecision(clean)}>Clean</span> | <span className={this.classDecision(working)}>Working</span>
+                    {this.isOwner(user)}
+                </div>
             </div>
         )
+    }
+        
 }
 
 export default Review
