ed21679b5358d658f221e9d0604d7d7370ae6066
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-15 17:38:33 -0400

    Clear localStorage.currentReview when it is set to null in the store to remove any flashing of previous review information before component updates.

--- src/reducers/currentReview.js
+++ src/reducers/currentReview.js
@@ -4,6 +4,7 @@ export default(state = null, action) => {
             localStorage.setItem('currentReview', JSON.stringify(action.review))
             return action.review
         case "CLEAR_CURRENT_REVIEW":
+            localStorage.setItem('currentReview', "null")
             return null
         default:
             return state
