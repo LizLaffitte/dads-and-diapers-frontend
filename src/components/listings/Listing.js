fda1b521b03e4c86c6d343616aa88d121b490154
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-15 19:27:26 -0400

    Render the remaining Listing chance/rating values.

--- src/components/listings/Listing.js
+++ src/components/listings/Listing.js
@@ -4,15 +4,19 @@ import {Link} from 'react-router-dom'
 class Listing extends Component {
     
     render(){
-        const {name, address, mens_chance, womens_chance} = this.props.listing.attributes
+        const {name, address, mens_chance, womens_chance, isolated_chance, clean_chance, working_chance} = this.props.listing.attributes
         const {id} = this.props.listing
         return(
             <div className="listing" >
                 <Link to={`/listings/${id}`}><h2>{name}</h2></Link>
                 <p>{address}</p>
                 <div className="rating-results">
-                <div>Men's: {mens_chance}</div>
-                <div>Women's: {womens_chance}</div>
+                    <h4>Ratings: </h4>
+                <div>Men's: {mens_chance}/100</div>
+                <div>Women's: {womens_chance}/100</div>
+                <div>Private: {isolated_chance}/100</div>
+                <div>Clean: {clean_chance}/100</div>
+                <div>Working: {working_chance}/100</div>
                 </div>
             </div>
         )
