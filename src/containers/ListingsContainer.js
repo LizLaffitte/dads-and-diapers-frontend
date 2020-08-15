304284ee757c08c0f4cff0493c5abd3b8c680136
Liz Laffitte <laffitte.digital@gmail.com>
2020-08-15 17:31:12 -0400

    Implements Reviews and ListingsContainer to clean up Main Container and group related Routes.

--- src/containers/ListingsContainer.js
+++ src/containers/ListingsContainer.js
@@ -1,38 +1,54 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchListings} from '../actions/listingActions'
-// import ListingContainer from './ListingContainer'
+import {Switch, Route} from 'react-router-dom'
+import {fetchListings, setCurrentListing, clearCurrentListing} from '../actions/listingActions'
+import {fetchReviews} from '../actions/reviewActions'
+import ListingCard from './ListingCard'
 import Listings from '../components/listings/Listings'
-// import Reviews from '../components/reviews/Reviews'
+import NewListingFormWrapper from '../containers/NewListingFormWrapper'
+import EditListingFormWrapper from '../containers/EditListingFormWrapper'
+
 
 
 class ListingsContainer extends Component {
-    // componentDidMount(){
-    //     this.props.fetchListings()
-    // }
-    // findListing = (id) => {
-    //     let listing = this.props.listings.find(listing => listing.id == id)
-    //     if(listing) {
-    //         this.props.setCurrentListing(listing)
-    //         return listing
-    //     } else {
-    //         return null
-    //     }
-        
-    // }
+    componentDidMount(){
+        this.props.fetchListings()
+        this.props.fetchReviews()
+    }
+    findListing = (id) => {
+        let listing = this.props.listings.find(listing => listing.id === id)
+        if(listing !== this.props.currentListing) {
+            this.props.setCurrentListing(listing)
+            return listing
+        } else {
+            let listing = JSON.parse(localStorage.getItem('currentListing'))
+            return listing
+        }
+    }   
     render(){
+        const {listings} = this.props
         return(
-            <div>
-                {/* <Route exact path="/listings/:id" render={props => {
-                const listing = this.findListing("19")
-                return <ListingContainer {...props} listing={listing} />}
-                }/> */}
-                <Listings listings={this.props.listings} />
-                
+            <div className="ListingsContainer">
+                <Switch>
+                    <Route exact path='/listings/new' component={NewListingFormWrapper} />
+                    <Route exact path="/listings/:id/edit" render={props => {
+                             const listing = this.findListing(props.match.params.id)
+                        return <EditListingFormWrapper {...props} listing={listing} />}
+                    }/>
+                    <Route path="/listings/:id" render={props => {
+                             const listing = this.findListing(props.match.params.id)
+                        return <ListingCard {...props} listing={listing} />}
+                    }/>
+                    <Route exact path='/listings' render={props => {
+                        this.props.clearCurrentListing()
+                        return <Listings {...props} listings={listings} /> }
+                    }/>
+                </Switch>
+                            
             </div>
         )
     }
     
 }
 const mapStateToProps = ({listings}) => ({listings})
-export default connect(mapStateToProps, {fetchListings})(ListingsContainer)
+export default connect(mapStateToProps, {fetchListings, fetchReviews, setCurrentListing, clearCurrentListing})(ListingsContainer)
---
+++ src/containers/ReviewsContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import NewReviewFormWrapper from './NewReviewFormWrapper'
+import EditReviewFormContainer from './EditReviewFormContainer'
+import Reviews from '../components/reviews/Reviews'
+import {setCurrentReview} from '../actions/reviewActions'
+
+
+class ReviewsContainer extends Component {
+
+    findReview = (id) => {
+        let review = this.props.reviews.find(review => review.id === id)
+        if(review && review !== this.props.currentReview){
+            this.props.setCurrentReview(review)
+            return review
+        } else {
+            let review = JSON.parse(localStorage.getItem('currentReview'))
+            return review
+        }        
+    } 
+
+    render(){
+        const { listing, reviews } = this.props
+        return (
+        <div className="ReviewsContainer">
+        <Switch>
+            <  Route exact path={'/listings/:id'} render={props => {
+                return <Reviews {...props} listing={listing} reviews={reviews}  />
+            }} />
+            <Route exact path={'/listings/:id/reviews/new'} render={props => {
+                return <NewReviewFormWrapper {...props} listing={listing}/>
+            }} />
+            <Route exact path={'/listings/:listingid/reviews/:id/edit'} render={props => {
+                 const review = this.findReview(props.match.params.id)
+                 return <EditReviewFormContainer {...props} review={review} listing={listing}/>
+            }} />
+                           {/* <Route exact path={'/listings/:listingid/reviews/:id/delete'} render={props => {
+                            const listing = this.findListing(props.match.params.listingid)
+                            const review = this.findReview(props.match.params.id)
+                            return <EditReviewFormContainer {...props} review={review} listing={listing}/>
+                        }} /> */}
+        </Switch> 
+
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = ({reviews, currentReview}) => ({reviews, currentReview})
+export default connect(mapStateToProps, {setCurrentReview})(ReviewsContainer)
