456d71eab5bd542ab8331ca5a13ec02a2041ec61
lizlaffitte <laffitte.digital@gmail.com>
2020-10-04 14:42:13 -0400

    Incorporate search result component.

---
+++ src/components/SearchResult.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+function SearchResult(props) {
+        return (
+            <> 
+                {props.name} <span class="second">{props.attr}</span>
+            
+        </>
+        )
+        
+    }
+export default SearchResult
